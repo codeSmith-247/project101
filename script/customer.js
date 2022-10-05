acc9909348927ce0ca8b0ffc4f6fc51ff129c864
Adomah20 <adomahbernard20@gmail.com>
2022-10-05 11:47:02 +0000

    customer.js problem

--- script/customer.js
+++ script/customer.js
@@ -15,6 +15,7 @@ mymodal.create_input({
     type:'number',
     placeholder: 'Customer payment',
     disabled: true,
+    onchange: 'calculate_quantity()', 
 
 });
 
@@ -24,6 +25,7 @@ mymodal.create_input({
     type: 'number',
     placeholder: 'Amount per bag',
     disabled: true,
+    onchange: 'calculate_quantity()', 
 });
 
 mymodal.create_input({
@@ -63,4 +65,22 @@ mymodal.create_input({
 
 
 });
+function calculate_quantity() {
+    let amount_payed = mymodal.get_input('payment')    == ''? 1 : mymodal.get_input('payment');
+    let Amount_per_Bag  = mymodal.get_input('Amount_per_Bag')  == ''? 1 : mymodal.get_input('Amount_per_Bag');
+  
 
+    console.clear();
+    console.log(
+        `
+            amount_payed: ${amount_payed},
+            sack_price: ${Amount_per_Bag},
+           
+        `
+    )
+
+    let Quantity = parseFloat(amount_payed) / parseFloat(Amount_per_Bag);
+Quantity = Quantity.toFixed(2);
+   
+    mymodal.set_input('quantity', `${Quantity} bag(s) );
+    }
