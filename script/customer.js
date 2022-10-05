a091e4329708abb7b852131cf1ee43116ef94645
codesmith-247 <adedavid.tech@gmail.com>
2022-10-05 11:45:29 +0000

    helped bernard with the customer logic part1

--- script/customer.js
+++ script/customer.js
@@ -15,7 +15,7 @@ mymodal.create_input({
     type:'number',
     placeholder: 'Customer payment',
     disabled: true,
-
+    onchange: 'calculate_quantity();',
 });
 
 mymodal.create_input({
@@ -24,6 +24,7 @@ mymodal.create_input({
     type: 'number',
     placeholder: 'Amount per bag',
     disabled: true,
+    onchange: 'calculate_quantity();',
 });
 
 mymodal.create_input({
@@ -64,3 +65,21 @@ mymodal.create_input({
 
 });
 
+function calculate_quantity() {
+    let amount_payed    = mymodal.get_input('payment')    == ''? 1 : mymodal.get_input('payment');
+    let amount_per_bg   = mymodal.get_input('Amount_per_Bag')  == ''? 1 : mymodal.get_input('Amount_per_Bag');
+
+    console.clear();
+    console.log(
+        `
+            amount_payed: ${amount_payed},
+            amount_per_bg: ${amount_per_bg},
+        `
+    )
+
+    let quantity  = parseFloat(amount_payed) / parseFloat(amount_per_bg);
+
+    quantity = quantity.toFixed(2);
+
+    mymodal.set_input('quantity', `${quantity} bag(s)`);
+}
